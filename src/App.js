a433cfd79fd8e8dc5b3a810d29b9981859cbba3d
Olha-Fokanova <ponomarova_olga91@ukr.net>
2023-04-17 15:49:13 +0300

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://dainty-pudding-d474dc.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Olha Fokanova{" "}
+        </a>
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/Olha-Fokanova/shecodes-weather-app-react-2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
