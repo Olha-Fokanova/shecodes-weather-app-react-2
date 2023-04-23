462042e64741810d5bfe57dc3539715b72bb40a6
Olha-Fokanova <ponomarova_olga91@ukr.net>
2023-04-23 11:56:46 +0300

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a
             href="https://dainty-pudding-d474dc.netlify.app/"
             target="_blank"
             rel="noreferrer"
+            className="text-decoration-none"
           >
             Olha Fokanova{" "}
           </a>
@@ -20,6 +21,7 @@ export default function App() {
             href="https://github.com/Olha-Fokanova/shecodes-weather-app-react-2"
             target="_blank"
             rel="noreferrer"
+            className="text-decoration-none"
           >
             GitHub
           </a>
